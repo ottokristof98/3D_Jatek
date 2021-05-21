155bcbacec384529aae4ac8281dfa822cef6b535
Kiss Ottó Kristóf <79306681+ottokristof98@users.noreply.github.com>
2021-05-22 00:00:18 +0200

    Cube Mozgatás Commit
    A jelenlegi "Cube" (leendő játékos) x tengely mentén való mozgatása

--- 3D_Jatek/Assets/Jatekos.cs
+++ 3D_Jatek/Assets/Jatekos.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Jatekos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float sebesseg;
+    public float maxX;
+    public float minX;
+    public float Xvalt;
+    private Vector3 cel;
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, cel, sebesseg * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxX)
+        {
+                cel = new Vector3(transform.position.x + Xvalt, transform.position.y, transform.position.z);
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minX)
+        {
+                cel = new Vector3(transform.position.x - Xvalt, transform.position.y, transform.position.z);
+        }
+
     }
 }
