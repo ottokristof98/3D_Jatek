0fbb8b655bb0e2c65d9c4b2861564826cfee641e
Kiss Ottó Kristóf <79306681+ottokristof98@users.noreply.github.com>
2021-05-22 14:54:51 +0200

    Pause and Game Over menu Commit
    Létrehoztam a Canvas-on belül egy Pause és egy Game Over menüt. Ha 'Escape'-t nyomok, akkor a Pause Menü látható, lefagy addig a játék. A Continue gomb megnyomása után folytathatjuk a játékot.
    A GameOver menü akkor jelenik meg, amikor nekiütközünk az adott objektumnak ami szembejün velünk

--- 3D_Jatek/Assets/Scripts/Jatekos.cs
+++ 3D_Jatek/Assets/Scripts/Jatekos.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Jatekos : MonoBehaviour
 {
     public float sebesseg;
@@ -14,6 +15,8 @@ public class Jatekos : MonoBehaviour
     private Text szoveg;
     private float ido = 0;
     private int mp;
+    public GameObject gameOver;
+    public GameObject pauseMenu;
     private void Start(){
         szoveg = szamlalo.GetComponent<Text>();
         
@@ -27,6 +30,10 @@ public class Jatekos : MonoBehaviour
         }else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minX)
         {
                 cel = new Vector3(transform.position.x - Xvalt, transform.position.y, transform.position.z);
+        }else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
         }
         ido += Time.deltaTime;
         if(ido>1)
@@ -35,5 +42,27 @@ public class Jatekos : MonoBehaviour
             ido = 0;
         }
         szoveg.text = mp.ToString();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Akadaly"))
+        {
+            Debug.Log("Ütközés");
+            gameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Continue(){
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
     }
 }
