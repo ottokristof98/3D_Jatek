67215634eba6bf2f95094fd95fd43bc9c2dd4362
Kiss Ottó Kristóf <79306681+ottokristof98@users.noreply.github.com>
2021-05-22 14:07:43 +0200

    Score Commit
    Hozzáadtam a felülethez egy "utat", amin végigmennek az objektumok.
    Készítettem egy Canvast, amihez hozzáadtam egy Text-et, és ez másodpercenként növekszik.

--- 3D_Jatek/Assets/Scripts/Jatekos.cs
+++ 3D_Jatek/Assets/Scripts/Jatekos.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Jatekos : MonoBehaviour
 {
     public float sebesseg;
@@ -9,6 +9,15 @@ public class Jatekos : MonoBehaviour
     public float minX;
     public float Xvalt;
     private Vector3 cel;
+    public GameObject szamlalo;
+    private float mpSzamlalo;
+    private Text szoveg;
+    private float ido = 0;
+    private int mp;
+    private void Start(){
+        szoveg = szamlalo.GetComponent<Text>();
+        
+    }
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, cel, sebesseg * Time.deltaTime);
@@ -19,6 +28,12 @@ public class Jatekos : MonoBehaviour
         {
                 cel = new Vector3(transform.position.x - Xvalt, transform.position.y, transform.position.z);
         }
-
+        ido += Time.deltaTime;
+        if(ido>1)
+        {
+            mp++;
+            ido = 0;
+        }
+        szoveg.text = mp.ToString();
     }
 }
